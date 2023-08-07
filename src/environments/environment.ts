7f1ae832ecbbe8e420f989a795a48f0966519370
jwsharpe <jameswrightsharpe@gmail.com>
2023-08-07 02:30:04 -0400

    typo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authApiBaseUrl: 'https://my-sms-messenger-frontend.onrender.com/users/tokens',
-  dashboardApiBaseUrl: 'https://my-sms-messenger-frontend.onrender.com/api/v1',
+  authApiBaseUrl: 'https://my-sms-messenger-backend.onrender.com/users/tokens',
+  dashboardApiBaseUrl: 'https://my-sms-messenger-backend.onrender.com/api/v1',
 };
